e89b8c9e8ed94d0c06e3324ecf0264009dfe7ed0
zeba62 <zeba.sabih@gmail.com>
2025-11-09 22:22:31 +0530

    Checked any real number is Integer or not.

--- src/Basics/IsInteger.java
+++ src/Basics/IsInteger.java
@@ -1,4 +1,17 @@
 package Basics;
 
+import java.util.Scanner;
+
 public class IsInteger {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter any Number: ");
+        double n = sc.nextDouble();
+        int x= (int)n;//typecast
+        if (n-x>0){
+            System.out.println("Not an integer");
+        }else {
+            System.out.println("It is an integer");
+        }
+    }
 }
