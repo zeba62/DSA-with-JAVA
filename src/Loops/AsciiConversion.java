9f36ebd77a2e8adbb24f73eb91051406007a5684
zeba62 <zeba.sabih@gmail.com>
2025-11-12 21:11:16 +0530

    Ascii Conversion int to char.

---
+++ src/Loops/AsciiConversion.java
@@ -0,0 +1,11 @@
+package Loops;
+
+import java.lang.invoke.SwitchPoint;
+
+public class AsciiConversion {
+    public static void main(String[] args) {
+        for (int i=65;i<=90;i++){
+            System.out.println(i+ " " +(char)i);
+        }
+    }
+}
