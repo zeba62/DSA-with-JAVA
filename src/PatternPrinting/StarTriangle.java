4457203bab3768240c666280b7b789f4eb178b60
zeba62 <zeba.sabih@gmail.com>
2025-12-04 00:57:40 +0530

    Star Triangle

---
+++ src/PatternPrinting/StarTriangle.java
@@ -0,0 +1,18 @@
+package PatternPrinting;
+
+import java.util.Scanner;
+
+public class StarTriangle {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter any value in n: ");
+        int n= sc.nextInt();
+        for (int i=1; i<=n;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print("*"+ " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
