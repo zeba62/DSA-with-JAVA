d8b3ca282b8a08e801e476b4e5d8f259b8e5aa3a
zeba62 <zeba.sabih@gmail.com>
2025-12-05 22:24:59 +0530

    Number Triangle , Start Triangle, Aphabet with number triangle

--- src/PatternPrinting/StarTriangle.java
+++ src/PatternPrinting/StarTriangle.java
@@ -9,7 +9,12 @@ public class StarTriangle {
         int n= sc.nextInt();
         for (int i=1; i<=n;i++){
             for (int j=1;j<=i;j++){
-                System.out.print("*"+ " ");
+                if (i%2!=0){
+                    System.out.print(j+ " ");//number star
+            }else{
+                    System.out.print((char) (j+64)+ " ");//number star
+
+                }
             }
             System.out.println();
         }
