fc0ca05c63a4d9e5ae1b85a2f0c16dced6d4b429
zeba62 <zeba.sabih@gmail.com>
2025-12-07 23:53:34 +0530

    Start Cross and Floyd's Triangle.

---
+++ src/PatternPrinting/StarCross.java
@@ -0,0 +1,21 @@
+package PatternPrinting;
+
+import java.util.Scanner;
+
+public class StarCross {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter any number: ");
+        int n= sc.nextInt();
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=n;j++){
+                if (i==j || j==(n+1-i)){
+                    System.out.print("* ");
+                }else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
